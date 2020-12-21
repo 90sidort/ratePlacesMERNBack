062d53bbd9e2c2e7ac46395fc0b181f0483d5459
andrew <90sidort@gmail.com>
2020-12-21 23:30:56 +0100

    add about field

--- models/place.model.js
+++ models/place.model.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const placeSchema = new Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  about: { type: String, required: true },
+  description: { type: String, required: false },
   image: { type: String, required: true },
   address: { type: String, required: true },
   location: {
