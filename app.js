8a89baedb4af9c7ec52e26d1ddf1e2d843e8ad92
andrew <90sidort@gmail.com>
2020-12-27 22:53:00 +0100

    comment refactor

--- app.js
+++ app.js
@@ -14,19 +14,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PATCH, DELETE, PUT"
-//   );
-//   next();
-// });
-
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 app.use("/api/places", placesRouter);
 app.use("/api/users", userRouters);
